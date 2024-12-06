04d8a9997390ec99c6e60c8c5581e96c4011e9e7
mayurisamanta <samantamayuri@gmail.com>
2024-12-06 07:47:30 +0530

    Adding digital course kotlin-springboot project

---
+++ digital-course/src/main/kotlin/com/kotlin/digital/course/DigitalCourseApplication.kt
@@ -0,0 +1,11 @@
+package com.kotlin.digital.course
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DigitalCourseApplication
+
+fun main(args: Array<String>) {
+	runApplication<DigitalCourseApplication>(*args)
+}
---
+++ digital-course/src/test/kotlin/com/kotlin/digital/course/DigitalCourseApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kotlin.digital.course
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DigitalCourseApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
